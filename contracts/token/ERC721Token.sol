7b85069e9d4047d1c473bc85b4b355a4356b2d85
khanhoait.bka <khanhoait.bka@gmail.com>
2019-01-09 10:54:14 +0700

    refactor: update name token

--- contracts/token/ERC721Token.sol
+++ contracts/token/ERC721Token.sol
@@ -5,7 +5,7 @@ import "../ownership/Ownable.sol";
 /// @title Interface for contracts conforming to ERC-721: Non-Fungible Tokens
 /// @author Dieter Shirley <dete@axiomzen.co> (https://github.com/dete)
 
-contract ERC721Token is ERC721Full("SKU TOKEN", "SKL"), Ownable {
+contract ERC721Token is ERC721Full("SKULLY", "SKL"), Ownable {
     bytes4 constant InterfaceSignature_ERC721 = 0xd37c58cd;
     bytes4 constant InterfaceSignature_ERC721Enumerable = 0xd37e9d63;
     bytes4 constant InterfaceSignature_ERC721Metadata = 0xd37e139f;
