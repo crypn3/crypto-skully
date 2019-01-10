1217431bb96a3e6b8647e378ac6b68c0b1c84bb5
khanhoait.bka <khanhoait.bka@gmail.com>
2019-01-10 16:07:17 +0700

    feat: add google cloud storage

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # crypto-skully
 Crypto skully erc721
+
+
+export GOOGLE_APPLICATION_CREDENTIALS=service-account.json
--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,8 @@ const GeoPattern = require('geopattern');
 const fs = require('fs');
 const sha1 = require("sha1");
 const originSvg = require('./SKULLY-BANDANA-TEMPLATE-inside-nocolor');
+const { Storage } = require('@google-cloud/storage');
+
 
 async function generate(text, index) {
   const geo = await GeoPattern.generate(text);
@@ -9,16 +11,45 @@ async function generate(text, index) {
   let modifyStr = originSvg.toString();
   modifyStr = modifyStr.replace('<pattern id="hat"/>', `<pattern id="hat" width="10%" height="10%">${geoStr}</pattern>`);
 
-  await fs.writeFileSync("images/" + index.toString() + '.svg', new Buffer(modifyStr));
+  let fileName = "images/" + index.toString() + '.svg';
+  await fs.writeFileSync(fileName, new Buffer(modifyStr));
+  await upFile(fileName);
+}
+
+async function upFile(filename) {
+    const storage = new Storage();
+    // Uploads a local file to the bucket
+    await storage.bucket("skull").upload(filename, {
+        destination: filename
+    });
 }
 
 async function test() {
 
-    for (let i = 1; i < 10; i++) {
+    for (let i = 2; i < 3; i++) {
         let sha1Str = sha1(i.toString());
         await generate(sha1Str, i);
     }
 }
+
+async function listBuckets() {
+    // [START storage_list_buckets]
+    // Imports the Google Cloud client library
+    const {Storage} = require('@google-cloud/storage');
+
+    // Creates a client
+    const storage = new Storage();
+
+    // Lists all buckets in the current project
+    const [buckets] = await storage.getBuckets();
+    console.log('Buckets:');
+    buckets.forEach(bucket => {
+        console.log(bucket.name);
+    });
+    // [END storage_list_buckets]
+}
+
+// listBuckets();
 test();
 
 
