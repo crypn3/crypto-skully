19f3a8c90447f0729caa1348d811d08317a4ca08
VoTaKa <VoTaKa@users.noreply.github.com>
2019-01-23 17:51:08 +0700

    Add files via upload

--- contracts/fighting/LocationManagement.sol
+++ contracts/fighting/LocationManagement.sol
@@ -1,67 +1,234 @@
 pragma solidity >=0.4.24;
-
-contract LocationManagement{
-
-    address public manager;
+contract MapOwner{
+    address public manager; // address of admin
+    address public CEO;
+    address public CTO;
+    address public SFC; // address of Skull Fighting contract
     event TransferOwnership(address indexed _oldManager, address indexed _newManager);
+    event SetNewSFC(address _oldSFC, address _newSFC);
+
     modifier onlyManager() {
-        require(msg.sender == manager);
+        require(msg.sender == manager 
+        || msg.sender == SFC 
+        || msg.sender == CEO 
+        || msg.sender == CTO);
         _;
     }
-    function transferOwnership(address _newManager) external onlyManager returns(bool) {
+
+    constructor() public {
+        manager = msg.sender;
+        CEO = msg.sender;
+        CTO = msg.sender;
+    }
+
+    // transfer ownership of contract to new address
+    function transferOwnership(address _newManager) external onlyManager returns (bool) {
+        require(_newManager != address(0x0));
         manager = _newManager;
         emit TransferOwnership(msg.sender, _newManager);
         return true;
     }
-    
+
+    function setCEO(address _newCEO) external onlyManager returns (bool) {
+        require(_newCEO != address(0x0));
+        CEO = _newCEO;
+        emit TransferOwnership(msg.sender, _newCEO);
+        return true;
+    }
+
+    function setCTO(address _newCTO) external onlyManager returns (bool) {
+        require(_newCTO != address(0x0));
+        CTO = _newCTO;
+        emit TransferOwnership(msg.sender, _newCTO);
+        return true;
+    }
+
+    // Set new SF address
+    function setNewSF(address _newSFC) external onlyManager returns (bool) {
+        require(_newSFC != address(0x0));
+        emit SetNewSFC(SFC, _newSFC);
+        SFC = _newSFC;
+        return true;
+    }
+}
+
+contract LocationManagement is MapOwner{
+    // struct of one location, include: latitude and longitude
     struct Location{
         uint64 latitude;
         uint64 longitude;
     }
-    
-    Location[] locations;
 
-    event SetNewLocation(uint256 _newLocationId, uint64 _lat, uint64 _long);
+    Location[] locations; // all locations already have verified
+
+    // Current location belong to skull
+    mapping (uint256 => Location[]) locationsOfSkull;
+    // Show the Id of Skull, which is the boss of this location
+    mapping (uint256 => uint256) locationIdToSkullId;
+
+    event SetNewLocation(address indexed _manager, uint256 _newLocationId, uint64 _lat, uint64 _long);
     event UpdateLocationInfo(uint256 _locationId, uint64 _newLat, uint64 _newLong);
+    event DeleteLocation(uint256 _locationId, uint64 _lat, uint64 _long);
 
-    constructor () public {
-        manager = msg.sender;
-    }
+    event UserPinNewLocation(address indexed _manager, uint256 _newLocationId, uint64 _lat, uint64 _long);
+    event SetLocationToNewSkull(uint256 _skullId, uint64 _lat, uint64 _long, uint256 _oldOwnerId);
+    event DeleteLocationFromSkull(uint256 _skullId, uint64 _lat, uint64 _long);
 
     /// Only Contract's Manager can set new location.
-    function setNewLocation(uint64 _lat, uint64 _long) external onlyManager {
+    /// By default, when set new location, owner of this location is Skull[0]
+    function setNewLocation(uint64 _lat, uint64 _long) public onlyManager returns (uint256) {
         locations.push(Location({latitude: _lat, longitude: _long}));
         uint256 _newId = locations.length - 1; /// Id of new location
-        emit SetNewLocation(_newId, _lat, _long);
+        if(msg.sender != SFC)
+            emit SetNewLocation(msg.sender, _newId, _lat, _long);
+        else{
+            emit UserPinNewLocation(msg.sender, _newId, _lat, _long);
+        }
+        return _newId;
     }
 
     /// Only Contract's Manager can set many new locations.
-    function setNewLocations(uint64[] _arrLat, uint64[] _arrLong) external onlyManager {
+    function setNewLocations(uint64[] _arrLat, uint64[] _arrLong) public onlyManager {
         require(_arrLat.length == _arrLong.length);
         uint256 length = _arrLat.length;
         for(uint i = 0; i < length; i++)
         {
             locations.push(Location({latitude: _arrLat[i], longitude: _arrLong[i]}));
             uint256 _newLocationId = locations.length - 1; /// Id of new location
-            emit SetNewLocation(_newLocationId, _arrLat[i], _arrLong[i]);
-        }
-        
+            emit SetNewLocation(msg.sender, _newLocationId, _arrLat[i], _arrLong[i]);
+        }    
     }
 
-    function updateLocationInfo(uint256 _locationId, uint64 _newLat, uint64 _newLong) external onlyManager {
+    // Update location info
+    function updateLocationInfo(uint256 _locationId, uint64 _newLat, uint64 _newLong) public onlyManager {
         locations[_locationId].latitude = _newLat;
         locations[_locationId].longitude = _newLong;
         emit UpdateLocationInfo(_locationId, _newLat, _newLong);
     }
 
+    // Manager can delete illegal location
+    // We need to check this location have belong to what skull
+    // if no, we just delete it
+    // if yes, we need to delete mapping locationsOfSkull on this skull first, and then delete on array locations[]
+    // When done..
+    // We set mapping location of skull (last) to new position of location
+    function deleteLocation(uint256 _locationId) public onlyManager {
+        require(_locationId < locations.length);
+        uint256 arrLength = locations.length; // length of array locations[]
+        uint64 lat = locations[_locationId].latitude;
+        uint64 long = locations[_locationId].longitude;
+        // check:
+        if(_locationId != arrLength-1) // position in mid array
+        {
+            if(locationIdToSkullId[_locationId] == 0)
+            {
+                locations[_locationId] = locations[arrLength-1];
+            }
+            else {
+                deleteLocationFromSkull(locationIdToSkullId[_locationId], _locationId);
+                locations[_locationId] = locations[arrLength-1];
+            }
 
-    function getLocationInfo(uint256 _locationId) public view returns (uint64 lat, uint64 long) {
+            // set mapping location of skull (last) to new position of location
+            if(locationIdToSkullId[arrLength-1] != 0)
+            {   
+                locationIdToSkullId[_locationId] = locationIdToSkullId[arrLength-1];
+            }
+        }
+        else { // position on right and 
+            if(locationIdToSkullId[_locationId] != 0)
+            {
+                deleteLocationFromSkull(locationIdToSkullId[_locationId], _locationId);
+            }
+        } 
+        delete locations[arrLength-1]; // delete the last location
+        locations.length--; // sud length by 1
+        emit DeleteLocation(_locationId, lat, long);
+    }
+
+    // Delete many locations
+    function deleteLocations(uint64[] _lat, uint64[] _long) public onlyManager {
+        require(_lat.length == _long.length);
+        for(uint i = 0; i < _lat.length; i++)
+        {
+            for(uint j = 0; j < locations.length; j++)
+            {
+                if (_lat[i] == locations[j].latitude && _long[i] == locations[j].longitude)
+                {
+                    deleteLocation(j);
+                }
+            }
+        }
+    }
+
+    // Get location info by id from array locations[]
+    function getLocationInfoById(uint256 _locationId) internal view returns (uint64 lat, uint64 long) {
         require(_locationId < locations.length);
         Location storage location = locations[_locationId];
         return (location.latitude, location.longitude);
     }
 
+    // Get location info by id from array locations[]
+    function getLocationIdByLatLong(uint64 _lat, uint64 _long) public view returns (uint256 _id) {
+       for(uint i = 0; i < locations.length; i++)
+       {
+           if(_lat == locations[i].latitude && _long == locations[i].longitude){
+               return i;
+           }
+       }
+       return locations.length;
+    }
+
+    // Total locations are verified
     function getTotalLocations() public view returns (uint256) {
         return locations.length;
+    }
+
+    ///----------------------------------------------------------///
+
+    /// Get location information of Skull by index
+    function getLocationInfoFromSkull(uint256 _skullId, uint _index) public view returns (uint256 locationId, uint64 lat, uint64 long) {
+        Location storage location = locationsOfSkull[_skullId][_index];
+        locationId = getLocationIdByLatLong(location.latitude, location.longitude);
+        return (locationId, location.latitude, location.longitude);
+    }
+
+    /// Get all location of each Skull
+    function getTotalLocationOfSkull(uint256 _skullId) public view returns (uint) {
+        return locationsOfSkull[_skullId].length;
+    }
+
+    /// Get Skull ID by Location ID
+    function getLocationInfo(uint256 _locationId) public view returns(uint256 skullId, uint64 lat, uint64 long) {
+        uint64 latitude;
+        uint64 longitude;
+        (latitude, longitude) = getLocationInfoById(_locationId);
+        return (locationIdToSkullId[_locationId], latitude, longitude) ;
+    }
+
+    /// Set exist location to new skull
+    function setLocationToSkull(uint256 _skullId, uint64 _lat, uint64 _long, uint256 _defenceLocationId) public onlyManager{
+        locationsOfSkull[_skullId].push(Location({latitude: _lat, longitude: _long}));
+        locationIdToSkullId[_defenceLocationId] = _skullId; //set location to new skully
+        emit SetLocationToNewSkull(_skullId, _lat, _long, _defenceLocationId);
+    }
+
+    /// Delete exist location in old skull
+    function deleteLocationFromSkull(uint256 _skullId, uint256 _defenceLocationId) public onlyManager{
+        uint arrayLength = getTotalLocationOfSkull(_skullId);
+        Location memory defenceLocation;
+        (defenceLocation.latitude, defenceLocation.longitude) = getLocationInfoById(_defenceLocationId);
+        uint i; //find index of defenceLocationId in array of skull
+        for(i = 0; i < arrayLength; i++)
+        {
+            if(locationsOfSkull[_skullId][i].latitude == defenceLocation.latitude && locationsOfSkull[_skullId][i].longitude == defenceLocation.longitude)
+                break; // already found
+        }
+        locationsOfSkull[_skullId][i] = locationsOfSkull[_skullId][arrayLength-1];
+        delete locationsOfSkull[_skullId][arrayLength-1];
+        locationsOfSkull[_skullId].length--;
+        locationIdToSkullId[_defenceLocationId] = 0; // set skull of this location to 0
+
+        emit DeleteLocationFromSkull(_skullId, defenceLocation.latitude, defenceLocation.longitude);
     }
 }
--- contracts/fighting/SkullFighting.sol
+++ contracts/fighting/SkullFighting.sol
@@ -6,18 +6,11 @@ contract SkullFighting{
     /// We need 2 skulls ready to fight
     /// One skull has many location to pin;
     SkullCore public skullCore = SkullCore(0x6D4921f70EF7fA8836B39039f24CC9EfA46694F7);
-    PO8Token public po8 = PO8Token(0x8744a672D5a2df51Da92B4BAb608CE7ff4Ddd804);
+    PO8Token public po8 = PO8Token(0xE538Bf5735EF0c30a6A303D2fF6A61E083CD4c12);
 
-    LocationManagement public locationMgmt = LocationManagement(0xaE5c82b1656F8B417301039D8e0f4F6132D0B2E8);
+    LocationManagement public locationMgmt = LocationManagement(0x94214a7178C5ee9638C2011CA15BF1737aebaF02);
     /// Skull's rank shows the time a Skully can attack in each day.
     uint32[13] internal attackTimes = [3, 25, 15, 10, 8, 7, 6, 5, 4, 4, 4, 3, 3];
-
-    /// Struct of Location
-
-    struct Location{
-        uint64 latitude;
-        uint64 longitude;
-    }
 
     /// Begin time of game: Fri, 18 Jan 2019 00:00:00 GMT
     uint256 public constant beginTime = 1547769600000;
@@ -32,66 +25,24 @@ contract SkullFighting{
     // total success attack
     mapping (uint256 => uint64) internal totalSuccessAttacks;
 
-    // Current location belong to skull
-    mapping (uint256 => Location[]) locationsOfSkull;
-    // Show the Id of Skull, that is the boss of this location
-    mapping (uint256 => uint256) locationIdToSkullId;
-
-    event Battle(address owner, uint256 attackId, uint256 defenceId, bool win);
-    event SetLocationToNewSkull(uint256 _skullId, uint64 _lat, uint64 _long, uint256 _oldOwnerId);
-    event DeleteLocationFromSkull(uint256 _skullId, uint64 _lat, uint64 _long);
+    event Battle(address owner, uint256 attackId, uint256 defenceId, uint64 lat, uint64 long, bool win);
+    
     event BeginTimeOfSkull(uint256 _skullId, uint256 _time);
     event ResetAttackTimes(uint256 _skullId, uint256 _time);
 
-    /// Get location information of Skull by index
-    function getLocationInfoFromSkull(uint256 _skullId, uint _index) public view returns (uint64 lat, uint64 long) {
-        Location storage location = locationsOfSkull[_skullId][_index];
-        return (location.latitude, location.longitude);
-    }
-
-    /// Get all location of each Skull
-    function getTotalLocationOfSkull(uint256 _skullId) public view returns (uint) {
-        return locationsOfSkull[_skullId].length;
-    }
-
-    /// Get Skull ID by Location ID
-    function getLocationInfo(uint256 _locationId) public view returns(uint256 skullId, uint64 lat, uint long) {
+    // struct of one location, include: latitude and longitude
+    struct Location{
         uint64 latitude;
         uint64 longitude;
-        (latitude, longitude) = locationMgmt.getLocationInfo(_locationId);
-        return (locationIdToSkullId[_locationId], latitude, longitude) ;
     }
-
-    /// Set exist location to new skull
-    function setLocationToSkull(uint256 _skullId, uint64 _lat, uint64 _long, uint256 _defenceLocationId) internal{
-        locationsOfSkull[_skullId].push(Location({latitude: _lat, longitude: _long}));
-        locationIdToSkullId[_defenceLocationId] = _skullId; //set location to new skully
-        emit SetLocationToNewSkull(_skullId, _lat, _long, _defenceLocationId);
-    }
-
-    /// Delete exist location in old skull
-    function deleteLocationFromSkull(uint256 _skullId, uint256 _defenceLocationId) internal{
-        uint arrayLength = getTotalLocationOfSkull(_skullId);
-        Location memory defenceLocation;
-        (defenceLocation.latitude, defenceLocation.longitude) = locationMgmt.getLocationInfo(_defenceLocationId);
-        uint i; //find index of defenceLocationId
-        for(i = 0; i <= arrayLength; i++)
-        {
-            if(locationsOfSkull[_skullId][i].latitude == defenceLocation.latitude && locationsOfSkull[_skullId][i].longitude == defenceLocation.longitude)
-                break; // already found
-        }
-        locationsOfSkull[_skullId][i] = locationsOfSkull[_skullId][arrayLength-1];
-        delete locationsOfSkull[_skullId][arrayLength-1];
-        locationsOfSkull[_skullId].length--;
-
-        emit DeleteLocationFromSkull(_skullId, defenceLocation.latitude, defenceLocation.longitude);
-    }
+    
 
     /// Game play, Fighting battle of two skulls
     function skullFightWith(uint256 attackId, uint256 _defenceLocationId) internal returns (bool) {
         require(skullCore.ownerOf(attackId) == msg.sender, "Sender must be the skull's owner!");              
 
-        uint256 defenceSkullId = locationIdToSkullId[_defenceLocationId]; // Skull ID of enemy
+        uint256 defenceSkullId;
+        (defenceSkullId,,) = locationMgmt.getLocationInfo(_defenceLocationId); // Skull ID of enemy
         require(defenceSkullId != attackId, "Skull have to attack on another skull!");
 
         require(_defenceLocationId < locationMgmt.getTotalLocations(), "The ID location must less than total locations of Map"); // The ID location must less than total locations of Map 
@@ -99,35 +50,35 @@ contract SkullFighting{
         uint256 attackPower; // Attack power of user's skull
         uint256 defencePower; // Defence power of the enemy
         Location memory defenceLocation; // Location attacking
-        (defenceLocation.latitude, defenceLocation.longitude) = locationMgmt.getLocationInfo(_defenceLocationId);
+        (,defenceLocation.latitude, defenceLocation.longitude) = locationMgmt.getLocationInfo(_defenceLocationId);
         /// get attack power of Skully;
         (,attackPower,,,) = skullCore.getSkull(attackId);
 
         /// attack to the new location
-        if(locationIdToSkullId[_defenceLocationId] == 0)
+        if(defenceSkullId == 0)
         {
-            setLocationToSkull(attackId, defenceLocation.latitude, defenceLocation.longitude, _defenceLocationId);
-            emit Battle(msg.sender, attackId, defenceSkullId, true);
+            locationMgmt.setLocationToSkull(attackId, defenceLocation.latitude, defenceLocation.longitude, _defenceLocationId);
+            emit Battle(msg.sender, attackId, defenceSkullId, defenceLocation.latitude, defenceLocation.longitude, true);
             return true;
         }     
         /// attack to the location with the enemy
         else
         {
-            (,,defencePower,,) = skullCore.getSkull(locationIdToSkullId[_defenceLocationId]);
+            (,,defencePower,,) = skullCore.getSkull(defenceSkullId);
 
             if(attackPower > defencePower)
             {
-                setLocationToSkull(attackId, defenceLocation.latitude, defenceLocation.longitude, _defenceLocationId);
-                deleteLocationFromSkull(defenceSkullId, _defenceLocationId);
-                emit Battle(msg.sender, attackId, defenceSkullId, true);
+                locationMgmt.deleteLocationFromSkull(defenceSkullId, _defenceLocationId);
+                locationMgmt.setLocationToSkull(attackId, defenceLocation.latitude, defenceLocation.longitude, _defenceLocationId);
+                emit Battle(msg.sender, attackId, defenceSkullId, defenceLocation.latitude, defenceLocation.longitude, true);
                 return true;
             }
-            emit Battle(msg.sender, attackId, defenceSkullId, false);
+            emit Battle(msg.sender, attackId, defenceSkullId, defenceLocation.latitude, defenceLocation.longitude, false);
             return false;
         }
     }
 
-    function fight(uint256 attackId, uint256 defenceLocationId) public payable returns(bool) {
+    function fight(uint256 attackId, uint256 defenceLocationId) public returns(bool) {
         require(po8.balanceOf(msg.sender) >= 20000000000000000000);// Balance of attacker must greater than 20 PO8.
         //po8.approve(address(this), 20000000000000000000);// users do it themself!
         require(po8.allowance(msg.sender, address(this)) >= 20000000000000000000); // Attacker must be approve at least 20 PO8 before attack (Only one time).
@@ -135,7 +86,8 @@ contract SkullFighting{
         // attack times of skull between 0 - maximum base-on skull's rank
         require(canAttack(attackId));
 
-        uint256 tempLocationId = locationIdToSkullId[defenceLocationId]; // save a temp ID of defence location
+        uint256 tempLocationId;
+        (tempLocationId,,) = locationMgmt.getLocationInfo(defenceLocationId); // save a temp ID of defence location
         if(skullFightWith(attackId, defenceLocationId)) {
             if(tempLocationId != 0)
                 po8.transfer(msg.sender, 30000000000000000000); // if attack success, attacker win this location and 30 PO8;
@@ -199,5 +151,15 @@ contract SkullFighting{
     // get total success attack times of skull
     function getTotalSuccsessAttacks(uint256 skullId) public view returns (uint64) {
         return totalSuccessAttacks[skullId];
+    }
+
+    // users pin their own location to the map
+    function pinLocation(uint256 skullId, uint64 _lat, uint64 _long) external returns (bool) {
+        require(skullCore.ownerOf(skullId) == msg.sender, "Sender must be the skull's owner!");
+        uint256 _newLocationId;
+        _newLocationId = locationMgmt.setNewLocation(_lat, _long); // creat new location, and get this id location
+        locationMgmt.setLocationToSkull(skullId, _lat, _long, _newLocationId); // set this new location to chosen skull of msg.sender
+        currentAttackTimes[skullId] ++; // increase attack times in day
+        return true;
     }
 }
